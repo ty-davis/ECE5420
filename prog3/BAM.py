4288e643870e23449e268f2d73dda9a535fdbb94
Ty Davis <tydavis.cougarfan@gmail.com>
2025-10-16 11:56:04 -0600

    Finished prog 3 repairs

--- prog3/BAM.py
+++ prog3/BAM.py
@@ -5,7 +5,7 @@ import sys
 from scipy.special import erfc
 from multiprocessing import Pool, cpu_count
 
-N = 10**4
+N = 10**9
 Ep = 1
 SNR_dB = np.array([0, 2, 4, 6, 8, 10, 12])
 
@@ -13,13 +13,13 @@ def process_segment(args):
     start_ptr, amount, ep, n0, seed_offset = args
     np.random.seed(int(time.time()) + seed_offset)
     bits_segment = np.random.randint(0, 2, amount)
-    noise_segment = np.random.normal(0, np.sqrt(n0/2), amount)
+    noise_segment = np.random.randn(amount) * np.sqrt(n0/2)
     correct_segment = sum(((((bits_segment * 2 - 1) * ep) + noise_segment > 0).astype(int) == bits_segment).astype(int))
 
     return correct_segment
 
 
-def main():
+def main(argv):
     start = time.time()
     np.random.seed(int(time.time()))
     snr_linear = 10 ** (SNR_dB / 10)
@@ -27,11 +27,20 @@ def main():
     ber_theory = []
 
     num_processes = 10
+    tex_data = {
+        '$N_0$': [],
+        '$\\frac{E_p}{N_0}$': [],
+        '$\\frac{E_p}{N_0}$ in dB': [],
+        '$P_b$ in $Q$ function': [],
+        '$P_b$ Theoretical': [],
+        '$P_b$ Simulation': [],
+    }
 
     for snr in snr_linear:
         # SNR = Ep / (N0 / 2)
         # N0 = 2 * Ep / SNR
-        n0 = 2 * Ep / snr
+        n0 = Ep / snr
+        tex_data['$N_0$'].append(n0)
 
         # lets break it up so it doesn't eat all my RAM
         segment_amount = 10 ** 8
@@ -52,9 +61,16 @@ def main():
 
         correct_total = sum(results)
         ber_results.append((N - correct_total) / N)
+
         inQ_val = np.sqrt(2 * Ep / n0)
         ber_theory_val = 0.5 * erfc(inQ_val / np.sqrt(2))
         ber_theory.append(ber_theory_val)
+
+        tex_data['$\\frac{E_p}{N_0}$'].append(Ep / n0)
+        tex_data['$\\frac{E_p}{N_0}$ in dB'].append(10 * np.log10(Ep / n0))
+        tex_data['$P_b$ in $Q$ function'].append(inQ_val)
+        tex_data['$P_b$ Theoretical'].append(ber_theory_val)
+        tex_data['$P_b$ Simulation'].append((N - correct_total) / N)
 
         print(f"N0: {n0:.8f} Ep/N0: {Ep / n0:.3f} Ep/N0 in DB: {10 * np.log10(Ep / n0):.3f} inQ: {inQ_val:.8f} SNR: {snr:.2f} CORRECT: {correct_total} BER theory: {ber_theory_val:.5e} BER sim: {ber_results[-1]:.5e}")
         print(f"ELAPSED: {time.time() - start:.2f}s")
@@ -63,6 +79,17 @@ def main():
 
     print(ber_results)
     plot_data(ber_results, ber_theory)
+
+    if '-t' in argv:
+        print_tex_data(tex_data)
+
+def print_tex_data(data):
+    print(data)
+    for k, vs in data.items():
+        print(f"{k} & ", end="")
+        for v in vs:
+            print(f"{v:.3f} & ", end="")
+        print("\\ ")
 
 def Q(x, n):
     return 0.5 * erfc(x / np.sqrt(n)) 
@@ -83,8 +110,8 @@ def plot_data(data_sim, data_theory):
 
 def just_data():
     # calculated by an earlier run and preserved for the sake of convenience
-    d_sim = [0.158664578, 0.104024821, 0.056507059, 0.02300095, 0.006004842, 0.000781807, 3.4137e-05]
-    d_theory = [ 1.58655e-01, 1.04029e-01, 5.64953e-02, 2.30071e-02, 6.00439e-03, 7.82701e-04, 3.43026e-05 ]
+    d_sim = [0.078652517, 0.037506918, 0.012500043, 0.002390372, 0.00019147, 3.879e-06, 4e-09]
+    d_theory = [0.07864960352514258, 0.03750612835892601, 0.012500818040737566, 0.002388290780932807, 0.00019090777407599314, 3.87210821552205e-06, 9.006010350628787e-09]
 
 
     plot_data(d_sim, d_theory)
@@ -94,5 +121,5 @@ if __name__ == '__main__':
     if '-d' in sys.argv:
         just_data()
         exit()
-    main()
+    main(sys.argv)
 
